54fdc6fa9dbbdffb9e6d64b410fae43c4b103a24
The Lomb <thelomb@gmail.com>
2014-11-22 10:49:05 +0100

    add function description

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment you will take advantage of the scoping rules of
+## the R language and how they can be manipulated to preserve state inside
+## of an R object.
 
-## Write a short comment describing this function
-
+## This function create a special matrix object that offer caching 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special
+## matrix object returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then the
+## `cachesolve` should retrieve the inverse from the cache. Otherwise it will call
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        solve(x)
 }
