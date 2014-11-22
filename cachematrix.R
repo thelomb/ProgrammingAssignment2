39f63aaa775f4d597d505a084bbcdc07659dda24
The Lomb <thelomb@gmail.com>
2014-11-22 21:14:39 +0100

    function commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,36 @@
 ## the R language and how they can be manipulated to preserve state inside
 ## of an R object.
 
-## This function create a special matrix object that offer caching 
+## This function create a special matrix object
 makeCacheMatrix <- function(x = matrix()) {
         
+        ## when constructing matrix the inverted matrix emptied
+        invMatrix <- NULL
+        
+        ## set function
+        set <- function(y) {
+                x<<- y
+                invMatrix<<-NULL
+        }
+        
+        ## get function
+        get <- function (){
+                x
+        }
+        
+        ## set the inverse matrix
+        setInvMatrix <- function(inverse){
+                invMatrix<<- inverse
+        }
+        
+        ## get the inverse matrix
+        getInvMatrix <- function(){
+                invMatrix
+        }
+        
+        list(set = set, get = get,
+             setInvMatrix = setInvMatrix,
+             getInvMatrix = getInvMatrix)        
 }
 
 
@@ -12,7 +39,28 @@ makeCacheMatrix <- function(x = matrix()) {
 ## matrix object returned by `makeCacheMatrix` above. If the inverse has
 ## already been calculated (and the matrix has not changed), then the
 ## `cachesolve` should retrieve the inverse from the cache. Otherwise it will call
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        solve(x)
+
+        ## the inverse matrix is retrieve from the special matrix
+        invMatrix <- x$getInvMatrix()
+        
+        ## if it exists the cache matrix is retrieved
+        if(!is.null(invMatrix)) {
+                
+                message("getting inverted matrix cached data")
+                return(invMatrix)
+        }
+        
+        ## if inverse matrix does not exists, retrieve the special matrix
+        myMatrix <- x$get()
+        
+        ## Return a matrix that is the inverse of 'myMatrix'
+        myMatrix <- solve(myMatrix, ...)
+        
+        ## Set the inverse matrix in the special object
+        x$setInvMatrix(myMatrix)
+        
+        ## return the inverse matrix
+        myMatrix
 }
